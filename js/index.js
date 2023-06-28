c5a0280de560c6a480a9c198c13137201514746f
ROHIT KHARWAR <rohitkumar27699@gmail.com>
2023-06-28 17:27:12 +0530

    update done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 // Add an event listener to the resume buttons
 
-  
+GitHubCalendar(".calendar", "Rohit27698");
+
+// or enable responsive functionality:
+GitHubCalendar(".calendar", "Rohit27698", { responsive: true });
+
+// Use a proxy
+GitHubCalendar(".calendar", "your-username", {
+   proxy (username) {
+     return fetch(`https://your-proxy.com/github?user=${username}`)
+   }
+}).then(r => r.text())
