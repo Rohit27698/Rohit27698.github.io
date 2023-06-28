3af3b988cc88a3093a4c395d8d439280fc94d742
ROHIT KHARWAR <rohitkumar27699@gmail.com>
2023-06-28 17:56:47 +0530

    24 se uper

--- js/index.js
+++ js/index.js
@@ -5,9 +5,3 @@ GitHubCalendar(".calendar", "Rohit27698");
 // or enable responsive functionality:
 GitHubCalendar(".calendar", "Rohit27698", { responsive: true });
 
-// Use a proxy
-GitHubCalendar(".calendar", "your-username", {
-   proxy (username) {
-     return fetch(`https://your-proxy.com/github?user=${username}`)
-   }
-}).then(r => r.text())
