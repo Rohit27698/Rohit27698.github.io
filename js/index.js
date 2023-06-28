6b1accd85c510b3ffad13d7826eb44d748574d79
ROHIT KHARWAR <rohitkumar27699@gmail.com>
2023-06-28 18:06:08 +0530

    a2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-// Add an event listener to the resume buttons
 
-GitHubCalendar(".calendar", "Rohit27698");
-
-// or enable responsive functionality:
 GitHubCalendar(".calendar", "Rohit27698", { responsive: true });
 
